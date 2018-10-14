f42eba75aae3f906d9bf05fc122af51134eb6b2b
neagkv <neagkv@gmail.com>
2018-10-14 01:38:46 +0000

    pep8 specs

--- log_report.py
+++ log_report.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python2.7.12
 
-import psycopg2, datetime
+import psycopg2
+import datetime
 
 DB = "news"
 
+
 # Function to connect to the news database and run queries
 def psql_connection(query):
-	"""Connect to the PostGreSQL database, run selected query and return the result"""
+	"""Connect to the PostGreSQL database,
+	run selected query and return the result"""
 	try:
 		conn = psycopg2.connect(database=DB)
 		cur = conn.cursor()
@@ -19,6 +22,7 @@ def psql_connection(query):
 		results = cur.fetchall()
 		conn.close()
 		return results
+
 
 # Function to find the three most viewed articles
 def most_viewed_articles():
@@ -36,6 +40,7 @@ def most_viewed_articles():
 	print("Most viewed articles:")
 	for result in results:
 		print result[0] + " - " + str(result[1]) + " views"
+
 
 # Function to find the three most viewed authors
 def most_viewed_authors():
@@ -57,11 +62,13 @@ def most_viewed_authors():
 	for result in results:
 		print result[0] + " - " + str(result[1]) + " views"
 
+
 # Function to find the days with more than 1% errors on user requests
 def find_error_days():
 	"""Create query string, connect to the database and parse results"""
 	query = """
-		SELECT all_requests.day, (ROUND((error_requests.bad * 1000)/all_requests.good)/10)
+		SELECT all_requests.day,
+		(ROUND((error_requests.bad * 1000)/all_requests.good)/10)
 		AS percent
 		FROM all_requests
 		JOIN error_requests
@@ -82,4 +89,3 @@ if __name__ == '__main__':
 	most_viewed_authors()
 	print("")
 	find_error_days()
-
